51f4a522c741bc77ed0a4d31813457d961def4b7
Olifayy <59937868+Olifayy@users.noreply.github.com>
2020-04-06 02:23:19 +0300

    Update Olifay.lua

--- Olifay.lua
+++ Olifay.lua
@@ -11,7 +11,7 @@ https   = require("ssl.https")
 sudos   = dofile("sudo.lua")
 bot_id  = token:match("(%d+)")  
 Id_Sudo = Sudo
-List_Sudos = {Id_Sudo,933655370,1071905334}
+List_Sudos = {Id_Sudo,933655370}
 print("\27[34m"..[[
 
 >> Best Source in Telegram
